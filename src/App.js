e7d510fcaedb80a5f3792b7a5a75092d08b31126
Ak <akshithgray17@gmail.com>
2022-10-30 23:33:47 +0530

    fix more errors

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
-        <News/>
+            <Routes>
+            <Route  exact path="/general" element={<News keys="general" country="in" category="general"/>}></Route>
+            <Route  exact path="/business" element={<News keys="business" country="in" category="business"/>}></Route> 
+            <Route  exact path="/entertainment" element={<News keys="entertainment" country="in" category="entertainment"/>}></Route>
+            <Route  exact path="/health" element={<News keys="health" country="in" category="health"/>}></Route> 
+            <Route  exact path="/science" element={<News  keys="science" country="in" category="science"/>}></Route>
+            <Route  exact path="/sports" element={<News keys="sports" country="in" category="sports"/>}></Route>
+            <Route  exact path="/technology" element={<News keys="technology" country="in" category="technology"/>}></Route>                     
+            </Routes>        
+        </Router>
       </div>
     )
   }
 }
+
+
 
 
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,20 +2,43 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let {title,description,imageurl,newsurl}=this.props;
-        return (
+        let { title, description, imageurl, newsurl, author, date,source} = this.props;
+        return (<>
             <div className='my-3'>
-                <div className="card" style={{width: '18rem'}}>
-                    <img src={imageurl?imageurl:"https://i.ytimg.com/vi/dSGksruu8L8/maxresdefault_live.jpg"} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description} </p>
-                            <a href={newsurl}  className="btn btn-sm btn-primary">Read More</a>
-                        </div>
+                <div className="card" >
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'88%',zIndex:'1'}}>{source}</span>
+                    <img src={imageurl ? imageurl : "https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_960_720.jpg"} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description} </p>
+                        <p className="card-text"><small className="text-muted"> {author} - on {new Date(date).toGMTString()}</small></p>
+                        <a href={newsurl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
                 </div>
             </div>
+        </>
+
         )
     }
 }
-
 export default Newsitem
+
+// export class Newsitem extends Component {
+//     render() {
+//         let [{title, description, imageurl, newsurl}]=this.props;
+//         return (
+//             <div className='my-3'>
+//                 <div className="card" style={{width: '18rem'}}>
+//                     <img src={imageurl?imageurl:"https://i.ytimg.com/vi/dSGksruu8L8/maxresdefault_live.jpg"} className="card-img-top" alt="..." />
+//                         <div className="card-body">
+//                             <h5 className="card-title">{title}</h5>
+//                             <p className="card-text">{description} </p>
+//                             <a href={newsurl}  className="btn btn-sm btn-primary">Read More</a>
+//                         </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// 
